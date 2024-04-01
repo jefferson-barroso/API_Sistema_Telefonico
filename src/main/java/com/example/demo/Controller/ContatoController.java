7ee0530916f6e17aad7f0d89cc420e3a73c0d98f
Jefferson Barroso Freitas <jeferssonbarroso@hotmail.com>
2024-03-31 22:56:50 -0300

    Impelmentação de busca por tipo de contato

--- src/main/java/com/example/demo/Controller/ContatoController.java
+++ src/main/java/com/example/demo/Controller/ContatoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Model.Contato;
 import com.example.demo.Repositories.ContatoRepository;
+import com.example.demo.enums.TipoContato;
 
 @RestController
 @RequestMapping("/telefones")
@@ -30,10 +32,16 @@ public class ContatoController {
     	
     }
 	
+	 @GetMapping("/tipo/{tipoContato}")
+	    public List<Contato> getContatosByTipo(@PathVariable TipoContato tipoContato) {
+	        return contatoRepository.findByTipoContato(tipoContato);
+	    }
+	
 	@ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Contato adicionar(@RequestBody Contato contato) {
         return  contatoRepository.save(contato);
     }
+	
 
 }
--- src/main/java/com/example/demo/Repositories/ContatoRepository.java
+++ src/main/java/com/example/demo/Repositories/ContatoRepository.java
@@ -1,11 +1,14 @@
 package com.example.demo.Repositories;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.Model.Contato;
+import com.example.demo.enums.TipoContato;
 
 @Repository
 public interface ContatoRepository extends JpaRepository<Contato,Long> {
-
+	 List<Contato> findByTipoContato(TipoContato tipoContato);
+	
 }
